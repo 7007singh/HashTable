403fc5e3651378ee1a3accc176c8ca29e8309049
7007singh <shw12021982@gmail.com>
2023-02-22 12:51:29 +0530

    [Shweta]Add: Finding frequency of words in hash table using linked list

---
+++ HashTables/MyMapNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    internal class MyMapNode<K, V>
+    {
+        public int size;
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int hash = key.GetHashCode();
+            int position = hash % size;
+            return Math.Abs(position);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];
+            if(linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                    return item.Value;
+            }
+
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>()
+            { Key = key, Value = value };
+            if (linkedList.Count != 0)
+            {
+                foreach (KeyValue<K, V> item1 in linkedList)
+                {
+                    if (item1.Key.Equals(key))
+                    { 
+                        break;
+                    }
+                }
+            }
+            linkedList.AddLast(item); 
+        }
+        public bool Exists(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            foreach(KeyValue<K,V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Display()
+        {
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string res = element.ToString();
+                        if (res != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+    }
+}
--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,31 @@ namespace HashTables
 {
     internal class Program
     {
+        public static void CountFrequencyOfWords()
+        {
+            string paragraph = "To be or not to be";
+            MyMapNode<string, int> hashTable = new MyMapNode<string, int>(6);
+            string[] words = paragraph.ToLower().Split(' ');
+
+            foreach(string word in words)
+            {
+                if (hashTable.Exists(word))
+                {
+                    hashTable.Add(word, hashTable.Get(word) + 1);
+                }
+                else
+                {
+                    hashTable.Add(word, 1);
+                }
+                Console.WriteLine("Displaying after add operation");
+                hashTable.Display();
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Hash Table operations");
+            CountFrequencyOfWords();
+            Console.ReadLine();
         }
     }
 }
